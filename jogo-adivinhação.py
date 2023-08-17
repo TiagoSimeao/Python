161e79f98bf214ad13c175f9355ac79a6a215bf9
TiagoSimeao <141253274+TiagoSimeao@users.noreply.github.com>
2023-08-17 17:06:40 -0200

    Jogo da Adivinhação
    primeiro teste com python

---
+++ jogo-adivinhação.py
@@ -0,0 +1,19 @@
+print("*********************************")
+print("Bem Vindo ao Jogo da Adivinhação!")
+print("*********************************")
+
+numero_secreto = 12
+
+chute = input("Digite o seu numero:")
+chute_int = int(chute)
+
+print("/////////////////////////")
+print("Você digitou", chute_int)
+print("/////////////////////////")
+
+if(numero_secreto == chute_int):
+    print("Parabéns você acertou!")
+else:
+    print("Você errou!")
+
+print("///////////////////////////")
