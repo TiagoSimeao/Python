39645183f59b045d2dd031c7fcea68031fd200f1
TiagoSimeao <141253274+TiagoSimeao@users.noreply.github.com>
2023-08-18 16:28:56 -0200

    Repetição
    loop com tentativas

--- jogo-adivinhação.py
+++ jogo-adivinhação.py
@@ -3,23 +3,30 @@ print("Bem Vindo ao Jogo da Adivinhação!")
 print("*********************************")
 
 numero_secreto = 12
-chute = input("Digite o seu número:")
-chute_int = int(chute)
+total_de_tentativas = 5
+rodada = 1
 
-acertou = numero_secreto == chute_int
-chute_maior = chute_int > numero_secreto
-chute_menor = chute_int < numero_secreto
+while(rodada <= total_de_tentativas):
+    print("----Tentativa", rodada, "de", total_de_tentativas)
+    chute = input("Digite o seu número:")
+    chute_int = int(chute)
+    acertou = numero_secreto == chute_int
+    chute_maior = chute_int > numero_secreto
+    chute_menor = chute_int < numero_secreto
 
-print("Você digitou", chute_int)
+    print("Você digitou", chute_int)
 
-if(acertou):
-    print("Parabéns você acertou!")
-else:
-    if(chute_maior):
-        print("Você errou! Seu número é maior que o número secreto!")
-    elif(chute_menor):
-        print("Você errou! Seu número é menor que o número secreto!")
+    if(acertou):
+        print("Parabéns você acertou!")
+    else:
+        if(chute_maior):
+            print("Você errou! Seu número é maior que o número secreto!")
+        elif(chute_menor):
+            print("Você errou! Seu número é menor que o número secreto!")
 
-print("******************************")
-print("Fim do Jogo!")
-print("******************************")
+    rodada = rodada +1
+
+
+    print("******************************")
+    print("Fim do Jogo!")
+    print("******************************")
