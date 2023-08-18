6f0fe58945ac9cbb2e2e8af1cb5a6612f869d2b9
TiagoSimeao <141253274+TiagoSimeao@users.noreply.github.com>
2023-08-18 16:15:26 -0200

    Update jogo-adivinhação.py

--- jogo-adivinhação.py
+++ jogo-adivinhação.py
@@ -3,18 +3,26 @@ print("Bem Vindo ao Jogo da Adivinhação!")
 print("*********************************")
 
 numero_secreto = 12
-chute = input("Digite o seu numero:")
+chute = input("Digite o seu número:")
 chute_int = int(chute)
+acertou = numero_secreto == chute_int
+chute_maior = chute_int > numero_secreto
+chute_menor = chute_int < numero_secreto
 
-print("/////////////////////////")
+
+
 print("Você digitou", chute_int)
-print("/////////////////////////")
 
-if(numero_secreto == chute_int):
+
+if(acertou):
     print("Parabéns você acertou!")
 else:
-    print("Você errou!")
+    if(chute_maior):
+        print("Você errou! Seu número é maior que o número secreto!")
+    elif(chute_menor):
+        print("Você errou! Seu número é menor que o número secreto!")
 
-print("///////////////////////////")
+
+print("******************************")
 print("Fim do Jogo!")
-print("///////////////////////////")
+print("******************************")
