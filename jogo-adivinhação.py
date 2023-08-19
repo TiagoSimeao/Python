90f32adac9f881568c01a487e44e22061a862cef
TiagoSimeao <141253274+TiagoSimeao@users.noreply.github.com>
2023-08-19 15:28:23 -0200

    Digite um número de 1 a 10

--- jogo-adivinhação.py
+++ jogo-adivinhação.py
@@ -3,14 +3,18 @@ print("Bem Vindo ao Jogo da Adivinhação!")
 print("*********************************")
 
 
-numero_secreto = 12
+numero_secreto = 7
 total_de_tentativas = 5
 
 
 for rodada in range(1,total_de_tentativas + 1):
     print("---- Tentativa {} de {}". format(rodada, total_de_tentativas))
-    chute = input("Digite o seu número:")
+    chute = input("Digite um número entre 1 e 10:")
     chute_int = int(chute)
+
+    if(chute_int < 1 or chute_int > 10):
+        print("O número não está entre 1 e 10!")
+        continue
 
     acertou = numero_secreto == chute_int
     chute_maior = chute_int > numero_secreto
@@ -26,6 +30,7 @@ for rodada in range(1,total_de_tentativas + 1):
             print("Você errou! Seu número é maior que o número secreto!")
         elif(chute_menor):
             print("Você errou! Seu número é menor que o número secreto!")
+
 
 print("******************************")
 print("Fim do Jogo!")
