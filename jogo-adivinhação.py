43ce1037107308c3233d2eac0fc557ee29756d53
TiagoSimeao <141253274+TiagoSimeao@users.noreply.github.com>
2023-08-20 21:21:01 -0200

    random
    numero secreto aleatório

--- jogo-adivinhação.py
+++ jogo-adivinhação.py
@@ -1,19 +1,21 @@
+import random 
+
 print("*********************************")
 print("Bem Vindo ao Jogo da Adivinhação!")
 print("*********************************")
 
-
-numero_secreto = 7
+numero_secreto = (random.randrange(1,101))
 total_de_tentativas = 5
 
+print(numero_secreto)
 
 for rodada in range(1,total_de_tentativas + 1):
     print("---- Tentativa {} de {}". format(rodada, total_de_tentativas))
-    chute = input("Digite um número entre 1 e 10:")
+    chute = input("Digite um número entre 1 e 100:")
     chute_int = int(chute)
 
-    if(chute_int < 1 or chute_int > 10):
-        print("O número não está entre 1 e 10!")
+    if(chute_int < 1 or chute_int > 100):
+        print("O número não está entre 1 e 100!")
         continue
 
     acertou = numero_secreto == chute_int
