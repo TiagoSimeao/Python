62db364fd7a2d224b77a2d13e8a2913a55afc1ed
TiagoSimeao <141253274+TiagoSimeao@users.noreply.github.com>
2023-08-31 19:03:48 -0200

    Update forca.py

--- forca.py
+++ forca.py
@@ -3,3 +3,10 @@ def jogar():
     print("*****************************")
     print("Bem Vindo ao Jogo da Forca!")
     print("*****************************\n")
+
+    print("******************************")
+    print("Fim do Jogo!")
+    print("******************************\n")
+
+if(__name__ == "__main__"):
+    jogar()
