ce88f652d939671184f165087dd8d35521418eeb
TiagoSimeao <141253274+TiagoSimeao@users.noreply.github.com>
2023-08-31 19:05:27 -0200

    Update jogos.py

--- jogos.py
+++ jogos.py
@@ -1,26 +1,31 @@
 import forca
 import adivinhação
 
-print("*********************")
-print("Escolha o seu jogo!")
-print("**********************\n")
 
-jogo = 0
+def escolher_jogo():
+    print("*********************")
+    print("Escolha o seu jogo!")
+    print("**********************\n")
 
-while jogo == 1 or 2:
+    jogo = 0
 
-    print("(1) Jogo da Adivinhação (2) Jogo da Forca\n")
-    jogo = int(input("Escolha o Jogo:"))
+    while jogo == 1 or 2:
 
-    if(jogo == 1):
-        print("Iniciando Jogo da advinhação!")
-        adivinhação.jogar()
-        
+        print("(1) Jogo da Adivinhação (2) Jogo da Forca\n")
+        jogo = int(input("Escolha o Jogo:"))
 
-    elif(jogo == 2):
-        print("Iniciando Jogo da Forca!")
-        forca.jogar()
-        
-    else:
-        print("Você não digitou uma opção válida!")
-        continue
+        if(jogo == 1):
+            print("Iniciando Jogo da advinhação!")
+            adivinhação.jogar()
+            
+
+        elif(jogo == 2):
+            print("Iniciando Jogo da Forca!")
+            forca.jogar()
+            
+        else:
+            print("Você não digitou uma opção válida!")
+            continue
+
+if(__name__ == "__main__"):
+    escolher_jogo()
