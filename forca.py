3b282c0f024eb17d68a99ccfcb6dabc38f30213c
TiagoSimeao <141253274+TiagoSimeao@users.noreply.github.com>
2023-09-10 18:22:34 -0200

    Update forca.py

--- forca.py
+++ forca.py
@@ -4,6 +4,27 @@ def jogar():
     print("Bem Vindo ao Jogo da Forca!")
     print("*****************************\n")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        
+        chute = input("Qual a letra?")
+        
+        posição = 1
+        
+        for letra in palavra_secreta:
+
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(chute,posição))
+                
+            posição = posição+1
+
+            
+    
+
+
     print("******************************")
     print("Fim do Jogo!")
     print("******************************\n")
